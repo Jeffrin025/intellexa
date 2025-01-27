d2d591cfed61c353ab66a3b91414d72f5d6ad085
harr2104 <sharni9904@gmail.com>
2025-01-27 18:11:21 +0530

    hello

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BG from "../bg/bg";
 import "./Card.css";
+import tick from '../../assets/tick.png'
+import cross from '../../assets/cross.png'
 import amongus from "../../assets/cha.png"; 
 
 
@@ -22,6 +24,7 @@ const Card = () => {
       <BG />
       <section id="event">
         <div className="book-content">
+        <h2 className="topic">Technical Events</h2>
           <div className="vote-bg">
             <p></p>
             <div className="phone-front">
@@ -30,7 +33,7 @@ const Card = () => {
               <div className="phone-screen">
                 <div className="card flex">
                   {Array.from({ length: 9 }, (_, index) => {
-                    const cardClass = `cards r1 white 0${index + 1}`;
+                    const cardClass = `cards white 0${index + 1}`;
                     const route = `/${index + 1}`; 
                     return (
                       <div
@@ -39,21 +42,24 @@ const Card = () => {
                         onMouseEnter={() => setHoveredCard(index + 1)}
                         onMouseLeave={() => setHoveredCard(null)} 
                       >
-                        <img src={amongus} alt="" style={{ height: "70px" }} />
-                        <h3>Event {index + 1}</h3>
+                        <img src={amongus} alt="" style={{ height: "70px"}} />
+                        <h3 className="title">Event {index + 1}</h3>
                         {hoveredCard === index + 1 && (
-                          <div className="hover-images">
+                          <div className="hover-images" >
                             <img
-                              src={amongus}
+                              src={tick}
                               alt="Tick Mark"
                               className="tick-mark"
                               onClick={() => handleCardClick(route)} 
+                              style={{ height: "40px" ,marginLeft:"40px"}} 
                             />
+
                             <img
-                              src={amongus}
+                              src={cross}
                               alt="Cross Mark"
                               className="cross-mark"
                               onClick={handleCrossClick} 
+                              style={{height: "40px" }} 
                             />
                           </div>
                         )}
