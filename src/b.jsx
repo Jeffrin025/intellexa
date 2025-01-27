423112f13d15d0839d32a0928de224d427d30460
jeffron025 <jaffyjeffrin@gmail.com>
2025-01-27 16:37:06 +0530

    events

--- src/b.jsx
+++ src/b.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router-dom'; // Import useParams
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import BG from './component/bg/bg';
-
 import './b.css';
 
 const B = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
 
   return (
     <>
       <BG />
       <div className="b-content">
         <h1>Event {id}</h1>
-        <p>Details for event {id}</p>
+        <div className="outter">
+          <div className="inner">
+            <div className="navbar">Navbar Section</div>
+            <div className="contents">
+              <div className="left-pane">Left Pane</div>
+              <div className="main-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <br /><br />
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
