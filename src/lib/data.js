fbf90cf8813dc1835e0f0984da45d1363b53792d
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-02-02 09:15:47 +0530

    added card abstracts

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,11 @@
 import card_abstract from "../assets/card_abstract.jpeg"
 import card_abstract_two from "../assets/card_abstract_two.jpg"
+import card_abstract_three from "../assets/card_abstract_three.jpg"
+import card_abstract_four from "../assets/card_abstract_four.jpg"
+import card_abstract_five from "../assets/card_abstract_five.jpg"
+import card_abstract_six from "../assets/card_abstract_siz.jpg"
+import card_abstract_seven from "../assets/card_abstract_seven.jpg"
+import card_abstract_eight from "../assets/card_abstract_eight.jpg"
 import poster1 from '../assets/poster/poster1.png'
 import poster2 from '../assets/poster/poster2.png'
 import poster3 from '../assets/poster/poster3.png'
@@ -11,13 +17,13 @@ import poster8 from '../assets/poster/poster8.png'
 export const eventCards = [
     { img: card_abstract, title: "CyberQuest", tagline: "Race Against Time, Conquer the Treasure!", team_size: 3, slug: "cyberquest", type: "event" },
     { img: card_abstract_two, title: "Aadukalam", tagline: "DSA Battlefield", team_size: "2 - 3", slug: "aadukalam", type: "event" },
-    { img: card_abstract, title: "Database Design Duel", tagline: "Two Minds, One Database - Will Yours Lead the Way?", team_size: 2, slug: "database-duel-design", type: "event" },
-    { img: card_abstract_two, title: "Flutter Frenzy", tagline: "Flutter Your Way to Innovation", team_size: 1, slug: "flutter-frenzy", type: "workshop" },
-    { img: card_abstract_two, title: "Web Wizard Relay", tagline: "Code. Collaborate. Create.", team_size: 1, slug: "web-wizard-relay", type: "event" },
-    { img: card_abstract_two, title: "Pixel Verse", tagline: "Please provide tagline", team_size: 1, slug: "pixel-verse", type: "non-tech" },
-    { img: card_abstract_two, title: "Chamber of Secrets", tagline: "Time's Not Your Friend—But Your Mind Might Be!", team_size: 2, slug: "chamber-of-secrets", type: "non-tech" },
-    { img: card_abstract_two, title: "Pitch perfect", tagline: "Express Ideas, Inspire Minds", team_size: "3 - 5", slug: "pitch-perfect", type: "event" },
-    { img: card_abstract_two, title: "Beyond the Screen: 3D Web Design Mastery", tagline: "From Concept to Creation: Shaping Brands of Tomorrow.", team_size: 1, slug: "beyond-the-screen", type: "workshop" },
+    { img: card_abstract_three, title: "Database Design Duel", tagline: "Two Minds, One Database - Will Yours Lead the Way?", team_size: 2, slug: "database-duel-design", type: "event" },
+    { img: card_abstract_four, title: "Flutter Frenzy", tagline: "Flutter Your Way to Innovation", team_size: 1, slug: "flutter-frenzy", type: "workshop" },
+    { img: card_abstract_five, title: "Web Wizard Relay", tagline: "Code. Collaborate. Create.", team_size: 1, slug: "web-wizard-relay", type: "event" },
+    { img: card_abstract_six, title: "Pixel Verse", tagline: "Please provide tagline", team_size: 1, slug: "pixel-verse", type: "non-tech" },
+    { img: card_abstract_seven, title: "Chamber of Secrets", tagline: "Time's Not Your Friend—But Your Mind Might Be!", team_size: 2, slug: "chamber-of-secrets", type: "non-tech" },
+    { img: card_abstract_seven, title: "Pitch perfect", tagline: "Express Ideas, Inspire Minds", team_size: "3 - 5", slug: "pitch-perfect", type: "event" },
+    { img: card_abstract, title: "Beyond the Screen: 3D Web Design Mastery", tagline: "From Concept to Creation: Shaping Brands of Tomorrow.", team_size: 1, slug: "beyond-the-screen", type: "workshop" },
 ]
 
 export const eventData = {
