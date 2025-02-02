570b0ecfccb7f235d236363472875a0fb80d68b1
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-02-02 09:20:39 +0530

    scroll issue

--- src/page/event/Event.jsx
+++ src/page/event/Event.jsx
@@ -1,4 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import BG from "../../component/bg/bg";
 import { eventData } from "@/lib/data";
 import { Navbar } from '../../component/navbar';
@@ -7,6 +8,9 @@ import footer_abstract from '../../assets/footer_abstract.jpg';
 const Event = () => {
   const { eventName } = useParams();
   const event = eventData[eventName];
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!event) {
     return (
@@ -24,7 +28,7 @@ const Event = () => {
   return (
     <>
       <BG />
-      <div className="relative p-6 sm:p-12 md:p-16 font-bazingaItalic min-h-screen w-screen flex flex-col  items-center justify-center pb-40">
+      <div className="relative p-6 sm:p-12 md:p-16 font-bazingaItalic min-h-screen w-screen flex flex-col items-center justify-center pb-40">
         <Navbar />
 
         <div
@@ -41,35 +45,28 @@ const Event = () => {
             <h1 className="text-4xl md:text-5xl font-bazingaItalic text-white">{event.title}</h1>
             <p className="text-lg md:text-xl text-gray-300 mt-2 border-b-2 border-gray-500 pb-2">{event.tagline}</p>
 
-
-
             <div className="mt-6 text-justify">
               <h2 className="text-2xl text-white font-semibold tracking-wider">Team Size ({event.team_size})</h2>
               <h2 className="text-2xl text-white font-semibold tracking-wider">{event.timing}</h2>
               <h2 className="text-2xl text-white font-semibold tracking-wider">{event.venue}</h2>
-              <h2 className="text-lg md:text-xl text-gray-300 mt-2 ">Event Head : {event.contact}</h2>
-              <h2 className="text-lg md:text-xl text-gray-300 mt-2 ">Contact : {event.num}</h2>
+              <h2 className="text-lg md:text-xl text-gray-300 mt-2">Event Head : {event.contact}</h2>
+              <h2 className="text-lg md:text-xl text-gray-300 mt-2">Contact : {event.num}</h2>
               <div className="flex justify-center space-x-2 mt-4">
-                {
-                  event?.theme?.length > 0 ? (
-                    <button
-                      onClick={() => {
-                        const link = document.createElement("a");
-                        link.href = "/PitchPerfect.pptx";
-                        link.download = "PitchPerfect.pptx";
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-                      }}
-                      className="text-lg md:text-xl font-bazingaItalic text-white border-2 border-white py-2 px-6 rounded-lg hover:border-red-500 transition-all"
-                    >
-                      PPT FORMAT
-                    </button>
-
-                  ) : (
-                    null
-                  )
-                }
+                {event?.theme?.length > 0 ? (
+                  <button
+                    onClick={() => {
+                      const link = document.createElement("a");
+                      link.href = "/PitchPerfect.pptx";
+                      link.download = "PitchPerfect.pptx";
+                      document.body.appendChild(link);
+                      link.click();
+                      document.body.removeChild(link);
+                    }}
+                    className="text-lg md:text-xl font-bazingaItalic text-white border-2 border-white py-2 px-6 rounded-lg hover:border-red-500 transition-all"
+                  >
+                    PPT FORMAT
+                  </button>
+                ) : null}
                 <button
                   onClick={() => console.log('Register')}
                   className="text-lg md:text-xl font-bazingaItalic text-white border-2 border-white py-2 px-6 rounded-lg hover:border-red-500 transition-all"
@@ -102,6 +99,6 @@ const Event = () => {
       </div>
     </>
   );
-}
+};
 
 export default Event;
